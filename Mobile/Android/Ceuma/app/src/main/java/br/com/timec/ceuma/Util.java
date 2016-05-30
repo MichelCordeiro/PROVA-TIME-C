286d189fc551f749037c7bc225edb79962c133d2
Felipe Aragao <felipe.thiago10@gmail.com>
2016-05-30 17:41:33 -0300

    img do front

--- Mobile/Android/Ceuma/app/src/main/java/br/com/timec/ceuma/Util.java
+++ Mobile/Android/Ceuma/app/src/main/java/br/com/timec/ceuma/Util.java
@@ -1,24 +1,16 @@
 package br.com.timec.ceuma;
 
-import android.app.ProgressDialog;
-import android.util.Log;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -27,7 +19,7 @@ import java.util.List;
 public class Util {
 
     private String tipo;
-    String url = "http://192.168.1.101/";
+    String url = "http://128.0.8.155/";
 
     public String genereteUrl(Integer id) {
         url += "ws-agendamento.php?id=" + id;
